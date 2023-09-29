c41bb8f9fdcadea5219c40e3cd21392da8a237b2
Obony <faithnyabz69@gmail.com>
2023-09-29 12:44:50 +0300

    correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then((response) => {
-      const responseArr = [];
-      response.forEach((data) => {
-        responseArr.push({
-          status: 'success',
-          value: data,
-        });
-      });
-      return responseArr;
-    })
-    .catch((error) => [{
-      status: 'rejected',
-      value: error,
-    }]);
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
 }
